16c77a2ec4e0c472edfee3ae7f34f5f00ce56a1c
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 15:06:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,13 +13,17 @@ end
 
 def find_max_value(array)
   # Add your solution here
-  max = array[0]
   counter = 1
+  max     = array[0]
+  
+  
+  
   max
 end
 
 def find_min_value(array)
   # Add your solution here
-  min = array[0]
+  counter = 1
+  min     = array[0]
   min
 end
